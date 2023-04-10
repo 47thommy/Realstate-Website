991e8469b6c4fefd6ed20a8fbb40f91943a00176
Thomas <47thommy@gmail.com>
2023-04-10 16:26:10 +0300

    popular page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,17 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
-
+var swiperPopular = new Swiper(".popular__container", {
+  spaceBetween: 32,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 /*=============== VALUE ACCORDION ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
